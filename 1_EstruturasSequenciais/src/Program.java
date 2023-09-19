135e7de017f096dc613721f047f08a332140033b
Adam Dreyton <adam.dreyton@gmail.com>
2023-09-19 17:09:27 -0300

    Primeiros exs de estruturas sequenciais

---
+++ 1_EstruturasSequenciais/src/Program.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		int idade;
+		double altura;
+		String nome;
+		char sexo;
+		
+		//Colocar valores e imprimi-los
+		idade = 33;
+		altura = 1.65;
+		nome = "Bob";
+		sexo = 'M';
+		
+		System.out.printf("Nome: %s, Idade: %d, "
+				+ "Altura: %.3f, Sexo: %c%n", 
+				nome, idade, altura, sexo);
+		
+		System.out.println("Nome: " + nome + 
+				", Idade: " + idade + 
+				", Altura: " + altura + 
+				", Sexo: " + sexo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
