e9258cf9f1310e7317ce888d29b7cd55f4a859ff
Adam Dreyton <adam.dreyton@gmail.com>
2023-10-27 18:08:11 -0300

    Tratamento de LocalDate e LocalDateTime Post/Comment

--- 9_oo_parte4/src/ex2/application/Program.java
+++ 9_oo_parte4/src/ex2/application/Program.java
@@ -1,13 +1,22 @@
 package ex2.application;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import ex2.entities.Comment;
 import ex2.entities.Post;
 
 public class Program {
 	public static void main(String[] args) {
-		LocalDateTime dt1 = LocalDateTime.now();//data atual
+		LocalDateTime dt1 = LocalDateTime.parse("2023-10-27T15:53:14");
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		LocalDateTime dt2 = LocalDateTime.parse("27/10/2023 15:53:14", dtf);
+		
+		LocalDate d3 = LocalDate.of(2023, 10, 27);
+		
+		System.out.println(d3);
 		
 		Post p1 = new Post("Traveling to new zealand", "content1", 2,
 				dt1);
