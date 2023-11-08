fab9955838096d7a8ffbbe080ef50b2bcb08b4f9
Adam Dreyton <adam.dreyton@gmail.com>
2023-11-08 15:05:49 -0300

    Ex herança: Account

---
+++ 10_oo_parte5/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Account ba = new BusinessAccount(31234, "Alex", 3000.0, 300.0);
+		
+	}
+
+}
---
+++ 10_oo_parte5/src/entities/Account.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Account extends Object{
+	private Integer number;
+	private String holder;
+	protected Double balance;
+	
+	public Account() {
+		
+	}
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public void withdraw(double value) {
+		this.balance -= value;
+	}
+	
+	public void deposit(double value) {
+		this.balance += value;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		if(number <= 1000)
+			this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	
+}
